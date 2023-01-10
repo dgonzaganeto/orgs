63eac45f586debac31f2536a8784290c45d08594
Dionizio Gonzaga <dgonzaganeto@gmail.com>
2023-01-10 19:42:52 -0300

    separando layout

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : Activity() {
 
         override fun onCreate(savedInstanceState: Bundle?){
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+//            setContentView(R.layout.activity_main)
+//            val textView2 = findViewById<TextView>(R.id.textView2)
+//            textView2.text = "Cesta de Frutas."
+//            val descricao = findViewById<TextView>(R.id.descricao)
+//            descricao.text = "Laranja, Uva e Manga"
+//            val preco = findViewById<TextView>(R.id.preco)
+//            preco.text  = "19.99"
+
+            val reclyerview = findViewById<RecyclerView>(R.id.reclyerview)
+
         }
 }
